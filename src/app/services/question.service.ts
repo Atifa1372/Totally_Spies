7b3ea860ac48ee215e743c6b48c2ae297030906b
Veronika <veronika.wolff@its-stuttgart.de>
2024-07-12 11:00:42 +0200

    feat: start implementing question store & service

--- src/app/services/question.service.ts
+++ src/app/services/question.service.ts
@@ -1,3 +1,14 @@
-export class QuestionService {
+import {HttpClient} from "@angular/common/http";
+import {inject} from "@angular/core";
+import {environment} from "../../environment/environment";
 
+export class QuestionService {
+  private http: HttpClient = inject(HttpClient);
+  private api_url: string = environment.api_url;
+
+  getQuestionsByAmountAndCategoryId(amount_of_questions: number, category_id: number) {
+    return this.http.get(
+      `${this.api_url}/api/SchaffIT/questions/random?category=${category_id}&count=${amount_of_questions}`
+    )
+  }
 }
