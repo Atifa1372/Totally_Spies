0f53c895838f8b52631ad78d97e9e289e84bd17a
Veronika <veronika.wolff@its-stuttgart.de>
2024-07-15 12:51:09 +0200

    feat: further implementation of question store

--- src/app/services/question.service.ts
+++ src/app/services/question.service.ts
@@ -1,12 +1,13 @@
 import {HttpClient} from "@angular/common/http";
 import {inject} from "@angular/core";
 import {environment} from "../../environment/environment";
+import {Observable} from "rxjs";
 
 export class QuestionService {
   private http: HttpClient = inject(HttpClient);
   private api_url: string = environment.api_url;
 
-  getQuestionsByAmountAndCategoryId(amount_of_questions: number, category_id: number) {
+  getQuestionsByAmountAndCategoryId(amount_of_questions: number, category_id: number): Observable<any> {
     return this.http.get(
       `${this.api_url}/api/SchaffIT/questions/random?category=${category_id}&count=${amount_of_questions}`
     )
