be119a5e616378127b0bf0b8b689b8aa3d749f22
Veronika <veronika.wolff@its-stuttgart.de>
2024-07-15 13:00:01 +0200

    add: example header (AppComponent) u. path (routes)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {ScorePageComponent} from "./pages/score-page/score-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'score', component: ScorePageComponent},
+
+];
