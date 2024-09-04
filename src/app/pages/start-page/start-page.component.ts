fb6c9d830d8b877dc7e4d2270d3d16b18d141fb7
vwolff <veronika.wolff@its-stuttgart.de>
2024-09-04 12:42:05 +0200

    add: start-page

---
+++ src/app/pages/start-page/start-page.component.ts
@@ -0,0 +1,27 @@
+import {Component, inject, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-start-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './start-page.component.html',
+  styleUrl: './start-page.component.css'
+})
+export class StartPageComponent implements OnInit{
+  private router: Router = inject(Router);
+
+  async ngOnInit(): Promise<void> {
+    await this.sleep(3500);
+    this.redirect_to_category_select_page();
+  }
+
+  async sleep(ms: number): Promise<void> {
+    return new Promise(
+      (sleep) => setTimeout(sleep, ms));
+  }
+
+  redirect_to_category_select_page() {
+    this.router.navigate(['category-select']);
+  }
+}
