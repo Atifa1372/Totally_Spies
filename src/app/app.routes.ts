7da887236e061bc5a1433b4147ae85cd7734d1e3
vwolff <veronika.wolff@its-stuttgart.de>
2024-09-04 12:51:31 +0200

    add: routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import {ScorePageComponent} from "./pages/score-page/score-page.component";
+import {StartPageComponent} from "./pages/start-page/start-page.component";
+import {CategorySelectPageComponent} from "./pages/category-select-page/category-select-page.component";
 
 export const routes: Routes = [
+  {path: '', component: StartPageComponent},
   {path: 'score', component: ScorePageComponent},
-
+  {path: 'category-select', component: CategorySelectPageComponent},
 ];
