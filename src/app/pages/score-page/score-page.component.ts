4ec53f8b1d2eaa8999ddf0099de731b509286c6d
vwolff <veronika.wolff@its-stuttgart.de>
2024-09-04 12:53:02 +0200

    change: add header to score-page

--- src/app/pages/score-page/score-page.component.ts
+++ src/app/pages/score-page/score-page.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {EmojiComponent} from "../../components/emoji/emoji.component";
 import {ScoreComponent} from "../../components/score/score.component";
+import {HeaderComponent} from "../../components/header/header.component";
 
 @Component({
   selector: 'app-score-page',
   standalone: true,
   imports: [
     EmojiComponent,
-    ScoreComponent
+    ScoreComponent,
+    HeaderComponent
   ],
   templateUrl: './score-page.component.html',
   styleUrl: './score-page.component.css'
