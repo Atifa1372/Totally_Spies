9995804bfb966056f8108663f7832690c792baab
vwolff <veronika.wolff@its-stuttgart.de>
2024-09-26 23:22:17 +0200

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import {ScorePageComponent} from "./pages/score-page/score-page.component";
 import {StartPageComponent} from "./pages/start-page/start-page.component";
 import {CategorySelectPageComponent} from "./pages/category-select-page/category-select-page.component";
-import { EmojiComponent } from './components/emoji/emoji.component';
+import {CountSelectPageComponent} from "./pages/count-select-page/count-select-page.component";
+import {EmojiComponent} from "./components/emoji/emoji.component";
 
 export const routes: Routes = [
   {path: '', component: StartPageComponent},
   {path: 'score', component: ScorePageComponent},
   {path: 'category-select', component: CategorySelectPageComponent},
+  {path: 'count-select/:category-id', component: CountSelectPageComponent},
 ];
 
 @NgModule({
