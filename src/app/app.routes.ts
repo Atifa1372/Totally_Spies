c312e08ddfa286ae74a7def5c5c6844768315d21
vwolff <veronika.wolff@its-stuttgart.de>
2024-09-27 12:50:59 +0200

    style global

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import {ScorePageComponent} from "./pages/score-page/score-page.component";
 import {StartPageComponent} from "./pages/start-page/start-page.component";
 import {CategorySelectPageComponent} from "./pages/category-select-page/category-select-page.component";
 import {CountSelectPageComponent} from "./pages/count-select-page/count-select-page.component";
-import {EmojiComponent} from "./components/emoji/emoji.component";
 import {QuestionPageComponent} from "./pages/question-page/question-page.component";
 import {AnswerPageComponent} from "./pages/answer-page/answer-page.component";
 
@@ -18,7 +17,7 @@ export const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), EmojiComponent],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
