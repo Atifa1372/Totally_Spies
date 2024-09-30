c9f5a3f84510d36ad0d975f81673fc0a557014b6
Atifa <atefeahmadi979@gmail.com>
2024-09-30 08:26:50 +0200

    feat(frontend): add the select page

--- src/app/pages/category-select-page/category-select-page.component.ts
+++ src/app/pages/category-select-page/category-select-page.component.ts
@@ -1,8 +1,9 @@
-import {Component, inject} from '@angular/core';
-import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
-import {HeaderComponent} from "../../components/header/header.component";
-import {Router} from "@angular/router";
-import {SchaffITStore} from "../../stores/schaffIT.store";
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HeaderComponent } from '../../components/header/header.component';
+import { Router } from '@angular/router';
+import { SchaffITStore } from '../../stores/schaffIT.store';
+import { Category } from '../../interfaces/category.interface';
 
 @Component({
   selector: 'app-category-select-page',
@@ -13,20 +14,32 @@ import {SchaffITStore} from "../../stores/schaffIT.store";
     HeaderComponent
   ],
   templateUrl: './category-select-page.component.html',
-  styleUrl: './category-select-page.component.scss'
+  styleUrls: ['./category-select-page.component.scss']
 })
 export class CategorySelectPageComponent {
   public schaffIT_store = inject(SchaffITStore);
   private router: Router = inject(Router);
 
-  public category_form: FormGroup = new FormGroup<any>({
-    category: new FormControl([], Validators.required)
-  })
+  public categories: Category[] = [];
+  public selectedSubject: string = ''; // This will store the selected subject
 
+  public category_form: FormGroup = new FormGroup<any>({
+    category: new FormControl(null, Validators.required)
+  });
+
+
+  // This method will handle the subject selection
+  selectSubject(subject: string) {
+    this.selectedSubject = subject; // Store the selected subject
+  }
+
+  // This method will navigate to the next page
   select_category() {
     if (this.category_form.valid) {
       this.schaffIT_store.set_selected_category(this.category_form.get('category')?.value);
       this.router.navigate(['count-select']).then();
-    }
+    } 
   }
+
+
 }
